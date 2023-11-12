14693e6bfe675455a4d6a7ec8a764ce23160b6f4
Konstantin Varvarkin <varvarkin.k@gmail.com>
2023-11-13 01:00:58 +0300

    added test_go_to_the_studio_is_possible.py

--- qa_guru_python_8_15/pages/main_page.py
+++ qa_guru_python_8_15/pages/main_page.py
@@ -6,8 +6,10 @@ from selene import browser, be, have, by
 class MainPage:
     main_menu_about = browser.element('//a[.="ABOUT"]')
     main_menu_games = browser.element('//a[.="GAMES"]')
+    main_menu_studios = browser.element('//a[.="STUDIOS"]')
     about_section = browser.element('#about')
-    game_project_space_marine_2 = browser.element('[data-id="a82225f"]')
+    game_project_space_marine_2 = browser.element('//a[@href="https://www.focus-entmt.com/en/games/warhammer-40000-space-marine-2"]')
+    studio_3d_realms = browser.element('//a[@href="https://3drealms.com/"]')
 
     def open(self):
         with allure.step('Открыть https://saber.games/'):
@@ -41,3 +43,21 @@ class MainPage:
                          'https://www.focus-entmt.com/en/games/warhammer-40000-space-marine-2'):
             current_url = browser.driver.current_url
             assert current_url == 'https://www.focus-entmt.com/en/games/warhammer-40000-space-marine-2', f"Текущий url страницы: {current_url}"
+
+    def go_to_studios_section(self):
+        with allure.step('Перейти в раздел "Studios"'):
+            self.main_menu_studios.should(be.clickable).click()
+
+    def click_on_studio_3d_realms(self):
+        with allure.step('Нажать на "3D Realms"'):
+            self.studio_3d_realms.should(be.clickable).click()
+
+    def go_to_the_studio_is_possible(self):
+        with allure.step('Проверить, что в title указано "Home - 3DRealms"'):
+            browser.switch_to_tab(1)
+            title = str(browser.driver.title)
+            assert title == 'Home - 3DRealms', f'Заголовок страницы: {title}'
+        with allure.step('Проверить, что URL страницы: '
+                         'https://3drealms.com/'):
+            current_url = browser.driver.current_url
+            assert current_url == 'https://3drealms.com/', f"Текущий url страницы: {current_url}"
